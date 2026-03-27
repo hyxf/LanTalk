cefaa6e99e2481e1bfbf1d0d50d3e2d1ba886bc5
qiudongchao <qiudongchao@gome.com.cn>
2026-03-27 16:01:20 +0800

    init

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import tseslint from 'typescript-eslint'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import prettierConfig from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -13,11 +13,11 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
+      tseslint.configs.recommendedTypeChecked,
       react.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -26,17 +26,8 @@ export default defineConfig([
         ...globals.node,
       },
       parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: new URL('.', import.meta.url).pathname,
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
       },
     },
-    rules: {
-      eqeqeq: ['error', 'always'],
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-floating-promises': 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unused-expressions': 'error',
-    },
   },
-  prettierConfig,
 ])
